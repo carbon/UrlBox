096b974a74f0d61f168dac28ef561b61ad6c4c14
Jason Nelson <jason@carbonmade.com>
2021-08-20 16:42:22 -0500

    Seal SerializedProperty

--- src/UrlBox/Serialization/SerializedProperty.cs
+++ src/UrlBox/Serialization/SerializedProperty.cs
@@ -3,7 +3,7 @@ using System.Runtime.Serialization;
 
 namespace UrlBox.Serialization
 {
-    internal class SerializedProperty
+    internal sealed class SerializedProperty
     {
         private readonly PropertyInfo property;
 
